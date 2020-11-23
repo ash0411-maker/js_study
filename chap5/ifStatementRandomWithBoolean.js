fa56ca4f7fd247fd0b1a3c696fc8c19be115b8c2
ash <satoshi.0411@icloud.com>
2020-11-23 12:22:13 +0900

    boolean

---
+++ chap5/ifStatementRandomWithBoolean.js
@@ -0,0 +1,9 @@
+let num = Math.round(Math.random() * 100);
+console.log("現在の値：" + num);
+let cond = (num <= 40)
+if(cond) {
+  console.log("below 40");
+} else {
+  console.log("OVER 50");
+}
+console.log("DONE!!!!");
