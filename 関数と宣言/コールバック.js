2a61ed61ecf9f5584827cb5dbd8784849a6da434
Uetani <satoshi.0411@icloud.com>
2021-02-22 16:07:57 +0900

    コールバック関数

---
+++ 関数と宣言/コールバック.js
@@ -0,0 +1,21 @@
+
+function 高階関数(コールバック関数) {
+  コールバック関数();
+}
+
+// ===========================================
+const array = [1,2,3];
+
+const output = (value) => {
+  console.log(value)
+}
+
+array.forEach(output);
+
+// ===========================================
+
+
+const = array = [1,2,3];
+array.forEach((value) => {
+  console.log(value);
+});
