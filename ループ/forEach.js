6b33218ad0bdeb5e37e4f136ce9dd7e06fe7dac0
Uetani <satoshi.0411@icloud.com>
2021-02-22 16:24:30 +0900

    forEach

--- ループ/forEach.js
+++ ループ/forEach.js
@@ -2,3 +2,12 @@ const array = [1, 2, 3];
 array.forEach(currentValue => {
     // 配列の要素ごとに呼び出される処理
 });
+
+function sum(...nums) {
+  let total = 0;
+  nums.forEach(num => {
+    total += num
+  })
+  return total;
+}
+sum(1,2,3,4,5);
