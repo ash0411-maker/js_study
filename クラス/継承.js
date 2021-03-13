5cef829d7e5047bb3fda57ecc0147c51ef6e3c02
ash <satoshi.0411@icloud.com>
2021-03-13 21:39:11 +0900

    継承

---
+++ クラス/継承.js
@@ -0,0 +1,14 @@
+// extendsキーワードを使うことで既存のクラスを継承できます。 
+// 継承とは、クラスの構造や機能を引き継いだ新しいクラスを定義することです。
+
+class 子クラス extends 親クラス {
+
+}
+
+class Parent {
+
+}
+class Child extends Parent {
+
+}
+const instance = new Child();
