1038f82148e84aba01d89310b80ba21cff9a8685
ash <satoshi.0411@icloud.com>
2021-03-14 20:15:53 +0900

    then 失敗時のコールバック関数のみを登録

--- 非同期処理/promise_then.js
+++ 非同期処理/promise_then.js
@@ -8,3 +8,18 @@ function delay(timeoutMs) {
 // `then`メソッドで成功時のコールバック関数だけを登録
 delay(10).then(() => {
   console.log("10ミリ秒後に呼ばれる");
+
+
+function errorPromise(message) {
+  return new Promise((resolve, reject) => {
+      reject(new Error(message));
+  });
+}
+// 非推奨: `then`メソッドで失敗時のコールバック関数だけを登録
+errorPromise("thenでエラーハンドリング").then(undefined, (error) => {
+    console.log(error.message); // => "thenでエラーハンドリング"
+});
+// 推奨: `catch`メソッドで失敗時のコールバック関数を登録
+errorPromise("catchでエラーハンドリング").catch(error => {
+    console.log(error.message); // => "catchでエラーハンドリング"
+});
