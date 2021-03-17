03ebddcb559957037f24a28f0d4cfb8bc4bb0af7
Uetani <satoshi.0411@icloud.com>
2021-03-17 09:23:27 +0900

    promise インスタンス作成

--- 非同期処理/promise.js
+++ 非同期処理/promise.js
@@ -8,3 +8,29 @@ asyncPromiseTask().then(()=> {
 }).catch(() => {
   // 非同期処理が失敗したときの処理
 });
+
+
+
+// Promiseインスタンスの作成
+const executor = (resolve, reject) => {
+  // 非同期の処理が成功したときはresolveを呼ぶ
+  // 非同期の処理が失敗したときはrejectを呼ぶ
+};
+const promise = new Promise(executor);
+
+
+
+// このPromiseインスタンスのPromise#thenメソッドで、Promiseがresolve（成功）、reject（失敗）したときに呼ばれるコールバック関数を登録します。 thenメソッドの第一引数にはresolve（成功）時に呼ばれるコールバック関数、第二引数にはreject（失敗）時に呼ばれるコールバック関数を渡します。
+// `Promise`インスタンスを作成
+const promise = new Promise((resolve, reject) => {
+  // 非同期の処理が成功したときはresolve()を呼ぶ
+  // 非同期の処理が失敗したときにはreject()を呼ぶ
+});
+const onFulfilled = () => {
+  console.log("resolveされたときに呼ばれる");
+};
+const onRejected = () => {
+  console.log("rejectされたときに呼ばれる");
+};
+// `then`メソッドで成功時と失敗時に呼ばれるコールバック関数を登録
+promise.then(onFulfilled, onRejected);
