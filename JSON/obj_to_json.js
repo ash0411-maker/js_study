9fb0a20c1996edf7ae80fe44214bc2d660bb3ae7
ash <satoshi.0411@icloud.com>
2021-03-21 20:48:36 +0900

    json replacer引数

--- JSON/obj_to_json.js
+++ JSON/obj_to_json.js
@@ -4,3 +4,20 @@
 
 const obj = { id: 1, name: "js-primer", bio: null };
 console.log(JSON.stringify(obj)); // => '{"id":1,"name":"js-primer","bio":null}'
+
+
+
+// JSON.stringifyメソッドにはオプショナルな引数が2つあります。 
+// 第二引数はreplacer引数とも呼ばれ、関数あるいは配列を渡せます。 
+// 関数を渡した場合は引数にプロパティのキーと値が渡され、その返り値によって文字列に変換される際の挙動をコントロールできます。 
+// 次の例は値がnullであるプロパティを除外してJSONに変換するreplacer引数の例です。 
+// replacer引数の関数でundefinedが返されたプロパティは、変換後のJSONに含まれなくなります。
+
+const obj = { id: 1, name: "js-primer", bio: null };
+const replacer = (key, value) => {
+    if (value === null) {
+        return undefined;
+    }
+    return value;
+};
+console.log(JSON.stringify(obj, replacer)); // => '{"id":1,"name":"js-primer"}'
