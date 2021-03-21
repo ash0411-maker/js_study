a20c9f18fc8685f06677c0da4377bb53d773f27d
ash <satoshi.0411@icloud.com>
2021-03-21 20:35:12 +0900

    map 要素の追加と取り出し

--- Map/map.js
+++ Map/map.js
@@ -6,3 +6,17 @@
 const map = new Map([["key1", "value1"], ["key2", "value2"]]);
 // 2つのエントリーで初期化されている
 console.log(map.size); // => 2
+
+
+// 要素の追加と取り出し
+const map = new Map();
+// 新しい要素の追加
+map.set("key", "value1");
+console.log(map.size); // => 1
+console.log(map.get("key")); // => "value1"
+// 要素の上書き
+map.set("key", "value2");
+console.log(map.get("key")); // => "value2"
+// キーの存在確認
+console.log(map.has("key")); // => true
+console.log(map.has("foo")); // => false
