fca28a8250d8295ae37870c3c2dd63ba91a6282c
ash <satoshi.0411@icloud.com>
2021-03-21 20:45:33 +0900

    json try...catch

--- JSON/json_obj.js
+++ JSON/json_obj.js
@@ -7,3 +7,16 @@ const json = '{ "id": 1, "name": "js-primer" }';
 const obj = JSON.parse(json);
 console.log(obj.id); // => 1
 console.log(obj.name); // => "js-primer"
+
+
+// 与えられた文字列がJSON形式でパースできない場合は例外が投げられます。
+// また、実際のアプリケーションでJSONを扱うのは、外部のプログラムとデータを交換する用途がほとんどです。
+// 外部のプログラムが送ってくるデータが常にJSONとして正しい保証はありません。
+// そのため、JSON.parseメソッドは基本的にtry...catch構文で例外処理をするべきです。
+
+const userInput = "not json value";
+try {
+    const json = JSON.parse(userInput);
+} catch (error) {
+    console.log("パースできませんでした");
+}
